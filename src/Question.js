8599359b580629e1f4e6ae3f23b308a7514ac9f7
dev-kakash <mdatikasifkhanakash@gmail.com>
2021-08-19 11:39:08 +0600

    initial commit

---
+++ src/Question.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => handleToggle()}>
+          {!toggle ? <AiOutlinePlus /> : <AiOutlineMinus />}
+        </button>
+      </header>
+      {toggle && <p>{info}</p>}
+    </article>
+  );
+};
+
+export default Question;
